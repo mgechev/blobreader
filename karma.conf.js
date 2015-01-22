737320ba3203461cf3a20354218ba71f7399f110
mgechev <mgechev@gmail.com>
2015-01-22 17:43:01 +0200

    Update karma config

--- karma.conf.js
+++ karma.conf.js
@@ -15,6 +15,7 @@ module.exports = function (config) {
 
     // list of files / patterns to load in the browser
     files: [
+      './bower_components/blob/Blob.js',
       './src/index.js',
       './test/*.spec.js'
     ],
@@ -53,7 +54,7 @@ module.exports = function (config) {
     // start these browsers
     // available browser launchers:
     // https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['Firefox'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
