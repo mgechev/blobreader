a61a48a1c8dad62c7ddefbe333d7f49dbf005752
mgechev <mgechev@gmail.com>
2015-12-17 12:05:19 +0200

    Prepare for npm

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,23 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var karma = require('karma').server;
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
 
-gulp.task('minify', function () {
+gulp.task('build', function () {
   'use strict';
-  return gulp.src('./src/**/*.js')
+  return browserify(['./src/index.js'])
+    .bundle()
+    .pipe(source('index.js'))
+    .pipe(buffer())
+    .pipe(rename('index.js'))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('minify', ['build'], function () {
+  'use strict';
+  return gulp.src('./dist/index.js')
     .pipe(uglify())
     .pipe(rename('./blob-reader.min.js'))
     .pipe(gulp.dest('./dist'));
@@ -28,3 +41,4 @@ gulp.task('autoTest', function () {
 });
 
 gulp.task('default', ['minify', 'test']);
+
